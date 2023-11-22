234937f98faa67c290a9e7ac78b7da5b1964e9fd
dsingh68 <dksinghsindhora@gmail.com>
2023-11-22 19:16:32 +0530

    logger is added

--- demo/src/main/java/com/demo/demo/service/StudentService.java
+++ demo/src/main/java/com/demo/demo/service/StudentService.java
@@ -1,14 +1,16 @@
 package com.demo.demo.service;
 
 import com.demo.demo.model.Student;
-import lombok.extern.log4j.Log4j;
-import org.slf4j.Logger;
+//import org.slf4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class StudentService {
-
+    Logger logger
+            = Logger.getLogger(StudentService.class.getName());
 
     private Map<String, Student> studentDatabase = new HashMap<>();
 
@@ -17,17 +19,23 @@ public class StudentService {
     public void addStudent(String studentId, Student student) {
 
         studentDatabase.put(studentId, student);
+        logger.log(Level.INFO, "Data is added");
     }
 
     public Student getStudent(String studentId) {
+        logger.log(Level.INFO, "Data is show");
         return studentDatabase.get(studentId);
     }
 
+
+
     public void updateStudent(String studentId, Student updatedStudent) {
         studentDatabase.put(studentId, updatedStudent);
+        logger.log(Level.INFO, "Data is updated");
     }
 
     public void deleteStudent(String studentId) {
         studentDatabase.remove(studentId);
+        logger.log(Level.INFO, "Data is deleted");
     }
 }
